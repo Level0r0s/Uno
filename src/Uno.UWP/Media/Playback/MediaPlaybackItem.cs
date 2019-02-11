9ab43430fc16db7f6bfad731ed086e33483862db
Maxime Leboeuf <maxime.leboeuf@nventive.com>
2019-02-11 15:54:53 -0500

    Implement MediaPlaybackList to allow queuing media items

---
+++ src/Uno.UWP/Media/Playback/MediaPlaybackItem.cs
@@ -0,0 +1,14 @@
+﻿using Windows.Media.Core;
+
+namespace Windows.Media.Playback
+{
+	public partial class MediaPlaybackItem : IMediaPlaybackSource
+	{
+		public MediaSource Source;
+
+		public MediaPlaybackItem(MediaSource source)
+		{
+			Source = source;
+		}
+	}
+}
